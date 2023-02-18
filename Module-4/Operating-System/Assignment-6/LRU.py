b66a7afdbc3d159257592eb59ea6a1eec9c5e6c5
G0urav <heapbytes@pm.me>
2023-02-18 20:59:29 +0530

    LRU page replace

--- Module-4/Operating-System/Assignment-6/LRU.py
+++ Module-4/Operating-System/Assignment-6/LRU.py
@@ -1,13 +1,14 @@
 #!/bin/env python3
 
 def replace(inp, frames):    
-    #input_ = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
-    less = 0
-    print("FRAMES : ", frames)
-    for i in frames:
-        less = inp.index(i) if inp.index(i) < less else less
-    
-    return less
+
+    ff = []
+    for i in inp[::-1]:
+        if i not in ff and i in frames:
+            ff.append(i)
+        else:
+            pass
+    return ff[-1]
 
 def LRU(inp , pg_frame_count):
 
@@ -18,51 +19,31 @@ def LRU(inp , pg_frame_count):
     counter = 0
     for i in inp:
         counter += 1
+        new_inp = inp[:counter - 1]
         if(len(frames) < pg_frame_count):
             if i not in frames:
                 frames.append(i)
+                print(i, ' \t*', [j for j in frames])
                 fault += 1
             else:
+                print(i, ' \t' , [j for j in frames])
                 hit += 1
         else:
             if i in frames:
+                print(i, ' \t ', [j for j in frames])
                 hit += 1
             else:
-                change = replace(inp[:counter], frames)
-                print(change)
-    return 1, 1
+                change = replace(new_inp, frames)
+                frames[frames.index(change)] = i
+                print(i, ' \t*', [j for j in frames])
+                fault += 1
+
+    return hit, fault
 
 if __name__ == '__main__':
-    #input_ = '7 0 1 2 0 3 0 4 2 3 0 3 2'.split(' ')
+
     input_ = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
     pg_frame_count = int(input("Enter page frame count : "))
     hit, fault = LRU(input_, pg_frame_count)
     print(f'\nFor the given input: \n{input_}\n\nTotal hits = {hit}\nFault = {fault}')
 
-
-'''
-
-    for i in range(len(input_)+1):
-        new_inp = input_[:i+1]
-        if(len(frames) < pg_frame_count):
-            if input_[i] not in frames:
-                frames.append(input_[i])
-                print(i, ' \t*', [j for j in frames])
-                fault += 1
-            else:
-                print(i, ' \t ', [j for j in frames])
-                hit += 1
-        else:
-            if i not in frames:
-                #new_inp = input_[:i+1]
-                print("NeW EW : ", new_inp)
-                change = replace(new_inp, frames)
-                frames[frames.index(new_inp[change])] = i
-                print(i, ' \t*', [j for j in frames])
-                fault += 1
-            else:
-                print(i, ' \t ', [j for j in frames])
-                hit += 1
-    return hit, fault 
-
-'''
