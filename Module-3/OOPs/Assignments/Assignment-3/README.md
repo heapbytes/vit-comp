08296b396eafef985c4333c1db92789644e9f6dd
heapbytes <heapbytes@pm.me>
2023-03-14 22:51:05 +0530

    Func Overloading

---
+++ Module-3/OOPs/Assignments/Assignment-3/README.md
@@ -0,0 +1,4 @@
+## Assignment 3 
+
+> Write Java program to calculate area of triangle, square & circle using function overloading. <br />
+> Function parameter accept from user (Use function Overloading concepts and Inheritance).
---
+++ Module-3/OOPs/Assignments/Assignment-3/areaFuncOverloading.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Area{
+  
+}
+
+public class areaFuncOverloading {
+
+  //circle
+  public double area(float PIE, int r){
+    return PIE * ( r * r );
+  }
+
+  //triangle
+  public double area(int base, int height){
+    return (base * height) / 2;
+  }
+
+  //square
+  public double area(int side){
+    return side * side;
+  }
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    int r, base, height, side;
+    float PIE;
+
+    areaFuncOverloading ar = new areaFuncOverloading();
+
+    System.out.print("Enter PIE value : ");
+    PIE = sc.nextFloat();
+    System.out.print("Enter radius : ");
+    r = sc.nextInt();
+    System.out.println("Area of circle = " + ar.area(PIE, r) + "\n");
+
+    System.out.print("Enter side of a square : ");
+    side = sc.nextInt();
+    System.out.print("Area of square : " + ar.area(side) + "\n");
+
+    System.out.print("\nEnter height : ");
+    height = sc.nextInt();
+    System.out.print("Enter base : ");
+    base = sc.nextInt();
+    System.out.print("Area of Triangle : " + ar.area(base, height) + "\n");
+
+    sc.close();
+
+  }
+}
