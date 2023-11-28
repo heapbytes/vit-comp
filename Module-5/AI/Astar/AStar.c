e17aa0f06d3e06a9259918dea36a42827e0fb7de
heapbytes <heapbytes@pm.me>
2023-11-28 17:45:28 +0530

    sem 5 done

--- Module-5/AI/Astar/AStar.c
+++ Module-5/AI/Astar/AStar.c
@@ -6,6 +6,7 @@
 #define ROWS 5
 #define COLS 5
 
+//defines the node structure
 typedef struct Node {
     struct Node* parent;
     int position[2];
@@ -14,33 +15,45 @@ typedef struct Node {
     int f;
 } Node;
 
+//creates a new node
+//parent: the parent node, i.e the node from which this node is reached
+//row: the row number of the node
+//col: the column number of the node
 Node* createNode(Node* parent, int row, int col) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->parent = parent;
     newNode->position[0] = row;
     newNode->position[1] = col;
-    newNode->g = 0;
-    newNode->h = 0;
-    newNode->f = 0;
+    newNode->g = 0; //distance from the start node
+    newNode->h = 0; //heuristic value
+    newNode->f = 0; //total cost
     return newNode;
 }
 
+//checks if the given position is valid or not, i.e it is within the maze
 bool isValid(int row, int col) {
     return (row >= 0 && row < ROWS && col >= 0 && col < COLS);
 }
 
+//checks if the given position is an obstacle or not
 bool isObstacle(int maze[ROWS][COLS], int row, int col) {
     return (maze[row][col] == 1);
 }
 
+//checks if the given position is the destination or not
 bool isDestination(int row, int col, int end[2]) {
     return (row == end[0] && col == end[1]);
 }
 
+//calculates the heuristic value of the given position
+//uses the Euclidean distance formula
+//formula : sqrt((x1-x2)^2 + (y1-y2)^2)
 int calculateHValue(int current[2], int end[2]) {
     return (int)(pow((current[0] - end[0]), 2) + pow((current[1] - end[1]), 2));
 }
 
+//prints the path from the start node to the end node
+//works when the end node is reached
 void printPath(Node* current) {
     if (current == NULL) {
         return;
@@ -49,14 +62,21 @@ void printPath(Node* current) {
     printf("(%d, %d)\n", current->position[0], current->position[1]);
 }
 
+//implements the A* algorithm
 bool aStar(int maze[ROWS][COLS], int start[2], int end[2]) {
+    //creates first node with no parent, i.e the start node
+    //sets the g, h and f values to 0
     Node* startNode = createNode(NULL, start[0], start[1]);
     startNode->g = startNode->h = startNode->f = 0;
     
+    //creates the end node with no parent
+    //sets the g, h and f values to 0
     Node* endNode = createNode(NULL, end[0], end[1]);
     endNode->g = endNode->h = endNode->f = 0;
     
     // Create a min-heap (priority queue)
+    //stores the nodes in the priority queue
+    //rows * cols is the maximum number of nodes that is rows * cols
     Node* priorityQueue[ROWS * COLS];
     int queueSize = 0;
     
@@ -65,6 +85,7 @@ bool aStar(int maze[ROWS][COLS], int start[2], int end[2]) {
     
     while (queueSize > 0) {
         // Find the node with the lowest f-value in the priority queue
+        // wont run 1st time bcuz 1<1 is false"
         int minIndex = 0;
         for (int i = 1; i < queueSize; i++) {
             if (priorityQueue[i]->f < priorityQueue[minIndex]->f) {
@@ -121,6 +142,7 @@ int main() {
         {0, 0, 0, 0, 0}
     };
     
+    //creates 2 arrays to store the start and end positions
     int start[2], end[2];
     
     printf("Enter the start position (row, column): ");
--- Module-5/AI/CSP/CSP.java
+++ Module-5/AI/CSP/CSP.java
@@ -1,9 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-public class CSP {
 
-    public static void main(String[] args) {
+public class CSP {
+        public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         String s1 = "SATURN";
@@ -11,6 +11,10 @@ public class CSP {
         String s3 = "PLANETS";
         Map<Character, Integer> map = new HashMap<>();
 
+        //it basically just puts all the characters in the map
+        //with a value of -1
+        // like this: {S=-1, A=-1, T=-1, U=-1, R=-1, N=-1, P=-1, L=-1}
+        //this is done so that we can check if a character is already in the map
         for (int i = 0; i < s1.length(); i++) {
             if (!map.containsKey(s1.charAt(i)))
                 map.put(s1.charAt(i), -1);
@@ -24,8 +28,11 @@ public class CSP {
         for (int i = 0; i < s3.length(); i++) {
             if (!map.containsKey(s3.charAt(i)))
                 map.put(s3.charAt(i), -1);
-        }
+            }
 
+        //this is just to get all the unique characters in the map
+        //and put them in an array
+        //result is: [S, A, T, U, R, N, P, L]
         char[] unique = new char[map.size()];
         int k = 0;
         for (char c : map.keySet()) {
@@ -33,13 +40,15 @@ public class CSP {
         }
 
         boolean[] visited = new boolean[10];
-
         solution(0, map, unique, visited, s1, s2, s3);
-
         scan.close();
     }
 
     public static void solution(int index, Map<Character, Integer> map, char[] unique, boolean[] visited, String s1, String s2, String s3) {
+        //at start index is 0 so this if loop will not run
+        //when index is equal to unique.length, it means that we have
+        //assigned a value to all the characters in the map
+        //so we can now check if the equation is valid
         if (index >= unique.length) {
             int n1 = toNum(map, s1);
             int n2 = toNum(map, s2);
@@ -55,6 +64,23 @@ public class CSP {
             return;
         }
 
+        //this loop will run 10 times
+        //it will check if the character at unique[index] is already in the map
+        //if it is not in the map, it will assign a value to it
+        //and then call the solution function again with index + 1
+        //if it is in the map, it will not assign a value to it
+        //and then call the solution function again with index + 1
+        //this is done to check all the possible combinations
+        //for e.g if the character at unique[index] is S
+        //it will check if S is already in the map
+        //if it is not in the map, it will assign a value to it
+        //value assigned to s is 0
+        //then it will call the solution function again with index + 1
+        //now index is 1
+        //so it will check if the character at unique[index] is already in the map
+        //if it is not in the map, it will assign a value to it
+        //and likewise
+        //this is done to check all the possible combinations
         for (int i = 0; i <= 9; i++) {
             if (visited[i] == false) {
                 map.put(unique[index], i);
@@ -74,3 +100,4 @@ public class CSP {
         return num;
     }
 }
+
